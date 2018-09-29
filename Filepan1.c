70dcd2cca36348a94fbead8359642bab0bb9047d
theprover97 <theprover97@gmail.com>
2018-09-29 08:43:04 +0530

    Create Filepan1.c
    I dont get the purpose of this file.
    Please explain to me.

---
+++ Filepan1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+ 
+int main()
+{
+	int n,i,j,q,y,t,x,y,max,l;
+	scanf("%d",&n);
+	int ar[n];
+for(i=0;i<n;i++){
+	
+ scanf("%d",&ar[i]);
+ if(i==0)
+ max=ar[i];
+ else if(ar[i]>max)
+   max=ar[i];
+   }
+ for(i=1;i<=n;i++)
+ {
+ 	
+ 	for(j=0;j<n;j++)
+ 	{
+ 		if(ar[j]==i)
+ 		 {
+ 		 	l=j+1;
+ 		 	break;
+ 		 	}
+ 		 }
+ 		 if(l<n)
+  x=ar[l];
+      else
+       x=ar[1];
+    
+ 
+ 
+int arr[q];
+int y;
+	return 0;
+}
+ 
